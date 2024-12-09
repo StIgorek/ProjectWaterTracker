7375f324457b53dbe75ab1f0cf9a50a0c4e763bb
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-09 13:16:49 +0200

    server base setting

---
+++ server/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const PORT = 3000;
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
